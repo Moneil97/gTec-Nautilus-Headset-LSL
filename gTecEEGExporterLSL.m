2b25ff361d2eb4c0f8bda4f2f81b436cbc233304
Michael O'Neil <cam8059@yahoo.com>
2018-11-20 12:36:06 -0500

    Updated comments on matlab block, removed unused componets from simulink model

--- gTecEEGExporterLSL.m
+++ gTecEEGExporterLSL.m
@@ -1,5 +1,8 @@
 classdef gTecEEGExporterLSL < matlab.System
-    % Gets 18 EEG Channels from Lab Streaming Layer
+    %Takes the EEG Data output of the g.Nautilus Block provided by gTec 
+    %and outputs it to Lab Streaming Layer as 1x32 chunks
+    %made for 32 channel headsets, but will likely work for 64 as well
+    %with minor modifications
     
     methods(Access = protected)
         function setupImpl(~)
@@ -11,22 +14,20 @@ classdef gTecEEGExporterLSL < matlab.System
             %lib = lsl_loadlib();
             assignin('base', 'lib', evalin('base', 'lsl_loadlib()')); %load lsl
             
-            %info = lsl_streaminfo(lib,'gTec','EEG',32,500,'cf_float32','gTecHeadset1');
+            %info = lsl_streaminfo(lib,'gTec','EEG',32,500,'cf_double64','gTecHeadset1');
             assignin('base', 'info', evalin('base', "lsl_streaminfo(lib,'gTec','EEG',32,500,'cf_double64','gTecHeadset1')"))
             
             %outlet = lsl_outlet(info);
             assignin('base', 'outlet', evalin('base', 'lsl_outlet(info)'));
         end
         
+        %chunk is [8x32] of EEG Data
         function stepImpl(~, chunk)
             coder.extrinsic('evalin');
             coder.extrinsic('assignin');
             
-            %push chunk to LSL output stream
-            %assignin('base', 'chunk', transpose(chunk));
-            %evalin('base', 'outlet.push_chunk(double(chunk))');
-            
             %break up chunk into 8 samples and push each seperately
+            %[8x32] -> 8 [1x32] samples
             for x = 1:8
                 assignin('base', 'sample', chunk(x,:));
                 evalin('base', 'outlet.push_sample(double(sample))');
